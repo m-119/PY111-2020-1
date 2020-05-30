1a9e947773a18ac280723137c517d4ef1a498e52
Беляков Михаил <m-119@ya.ru>
2020-05-30 21:38:38 +0300

    a3 done

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,20 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
-
+    c = 0
+    for b in brackets_row:
+        if b == "(":
+            c += 1
+        elif b == ")":
+            c -= 1
+        if c < 0:
+            return False
+    if not c:
+        return True
     return False
+
+# если исходить из того что:
+#     открывающая скобка должна закрыться
+#     закрывающая не существует без открывающей идущей ранее
+
+
