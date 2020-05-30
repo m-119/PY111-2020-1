1dd50d059dadc21d95d69e72f0ca8c715e13a851
Беляков Михаил <m-119@ya.ru>
2020-05-30 21:29:49 +0300

    a2 done

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -5,14 +5,19 @@ Queue priorities are from 0 to 10
 """
 from typing import Any
 
+COUNT_PRIORITY = 11
+priority_deque = {p: [] for p in range(COUNT_PRIORITY)}
 
 def enqueue(elem: Any, priority: int = 0) -> None:
     """
     Operation that add element to the end of the queue
 
+    :param priority:
     :param elem: element to be added
     :return: Nothing
     """
+    global priority_deque
+    priority_deque[priority].append(elem)
     return None
 
 
@@ -22,6 +27,10 @@ def dequeue() -> Any:
 
     :return: dequeued element
     """
+    global priority_deque
+    for j in range(COUNT_PRIORITY):
+        if priority_deque[j]:
+            return priority_deque[j].pop(0)
     return None
 
 
@@ -29,9 +38,14 @@ def peek(ind: int = 0, priority: int = 0) -> Any:
     """
     Allow you to see at the element in the queue without dequeuing it
 
+    :param priority:
     :param ind: index of element (count from the beginning)
     :return: peeked element
     """
+    global priority_deque
+    if priority_deque.get(priority, False):
+        if ind < len(priority_deque.get(priority)):
+            return priority_deque.get(priority)[ind]
     return None
 
 
@@ -41,4 +55,6 @@ def clear() -> None:
 
     :return: None
     """
+    global priority_deque
+    priority_deque = {p: [] for p in range(COUNT_PRIORITY)}
     return None
