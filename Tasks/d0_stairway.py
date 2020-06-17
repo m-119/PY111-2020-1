016ee8625ee96ca942fba9e1aed4bb503da9b492
Беляков Михаил <m-119@ya.ru>
2020-06-17 13:15:27 +0300

    d0 done Задача с лестницей

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,5 +8,17 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: list of ints, where each int is a cost of appropriate step
     :return: minimal cost of getting to the top
     """
+
+    vizited_stairway = {1: stairway[0],
+                        2: stairway[1]}
+
+    for step, coast in enumerate(stairway[2:], start=2):
+        vizited_stairway[step + 1] = min(vizited_stairway[step],
+                                         vizited_stairway[step - 1]) + coast
+
+    return vizited_stairway[len(stairway)]
+
     print(stairway)
     return 0
+
+
