83b630387f390bb02718e50ed49bed539ba4afd9
Беляков Михаил <m-119@ya.ru>
2020-06-17 12:30:28 +0300

    c0 done

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,6 +5,15 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
+    if n < 0:
+        raise ValueError
+    elif n < 1:
+        return 0
+    elif n < 2:
+        return 1
+    else:
+        return fib_recursive(n-1) + fib_recursive(n-2)
+
     print(n)
     return 0
 
@@ -16,5 +25,21 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    result = 0
+    if n < 0:
+        raise ValueError
+    elif n < 1:
+        result = 0
+    elif n < 2:
+        result = 1
+    else:
+        cnt = 1
+        prev = [0, 1]
+        while cnt < n:
+            cnt += 1
+            result = prev[0] + prev[1]
+            prev.append(result)
+            prev.pop(0)
+
+    print(f"{str(n)} = {str(result)}")
+    return result
